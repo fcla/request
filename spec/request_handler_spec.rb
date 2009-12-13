5418d96ec7e2c560b729b2cd20c45ca7029c8447
Manny Rodriguez <manny@bianchi.(none)>
2009-12-13 15:08:44 -0500

    factored user test methods out

--- spec/request_handler_spec.rb
+++ spec/request_handler_spec.rb
@@ -1,65 +1,12 @@
 require 'request_handler'
 require 'pp'
+require 'helper'
 
 describe RequestHandler do
 
   before(:each) do
     DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/data/request.db")
     DataMapper.auto_migrate!
-  end
-
-  def add_op_user
-    user = User.new
-
-    user.attributes = {
-      :username => "op",
-      :password => "op",
-      :account => "FDA",
-      :is_operator => true,
-      :can_disseminate => true,
-      :can_withdraw => false,
-      :can_peek => false
-    }
-
-    user.save
-
-    return user
-  end
-
-  def add_privileged_user
-    user = User.new
-
-    user.attributes = {
-      :username => "priv",
-      :password => "priv",
-      :account => "FDA",
-      :is_operator => false,
-      :can_disseminate => true,
-      :can_withdraw => true,
-      :can_peek => true
-    }
-
-    user.save
-
-    return user
-  end
-
-  def add_non_privileged_user account = "FDA"
-    user = User.new
-
-    user.attributes = {
-      :username => "nopriv",
-      :password => "nopriv",
-      :account => account,
-      :is_operator => false,
-      :can_disseminate => false,
-      :can_withdraw => false,
-      :can_peek => false
-    }
-
-    user.save
-
-    return user
   end
 
   it "should enqueue a new disseminate request requested by an operator" do
