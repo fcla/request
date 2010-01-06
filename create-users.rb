dfeb80ad5b6fa6b7788ce38bdbe0e3aaa969331c
Manny Rodriguez <manny@bianchi.fcla.edu>
2010-01-06 11:00:10 -0500

    better sample users

--- create-users.rb
+++ create-users.rb
@@ -9,8 +9,8 @@ user_1 = User.new
 user_2 = User.new
 
 user_1.attributes = {
-  :username => "foo",
-  :password => "bar",
+  :username => "op",
+  :password => "op",
   :account => "FDA",
   :is_operator => true,
   :can_disseminate => true,
@@ -19,10 +19,10 @@ user_1.attributes = {
 }
 
 user_2.attributes = {
-  :username => "foo",
-  :password => "bar",
+  :username => "user",
+  :password => "user",
   :account => "FDA",
-  :is_operator => true,
+  :is_operator => false,
   :can_disseminate => true,
   :can_withdraw => false,
   :can_peek => false
@@ -30,24 +30,4 @@ user_2.attributes = {
 
 user_1.save!
 user_2.save!
-
-ieid1 = rand(1000)
-ieid2 = rand(1000)
-ieid3 = rand(1000)
-ieid4 = rand(1000)
-
-# disseminate then peek for 1
-RequestHandler.enqueue_request user_1, :disseminate, ieid1
-sleep 2
-RequestHandler.enqueue_request user_1, :peek, ieid1
-
-# not authorized withdraw request for 2
-RequestHandler.enqueue_request user_1, :withdraw, ieid2
-
-# authorized withdraw request for 3
-id = RequestHandler.enqueue_request user_1, :withdraw, ieid3
-RequestHandler.authorize_request id, user_2
-
-# peek for 4
-RequestHandler.enqueue_request user_1, :peek, ieid4
 
