344fa2e07cf81c47fac9beedb3e4a1699509fb4b
Manny Rodriguez <manny@bianchi.fcla.edu>
2010-04-26 15:58:06 -0400

    dispatch program writes package tracker event

--- dispatch-workspace.rb
+++ dispatch-workspace.rb
@@ -3,6 +3,7 @@
 require 'db/request'
 require 'dispatch'
 require 'daitss/config'
+require 'package_tracker'
 
 WORKSPACE = ENV["WORKSPACE"]
 
@@ -10,6 +11,29 @@ Daitss::CONFIG.load ENV['CONFIG']
 DataMapper.setup :default, Daitss::CONFIG['database-url']
 
 #for any request ieid for which there is no wip in the workspace, dispatch a "sub-wip" for that request
+
+def create_op_agent
+  p = Program.new
+
+  p.attributes = {
+    :description => "request dispatch program",
+    :active_start_date => Time.at(0),
+    :active_end_date => Time.now + (86400 * 365),
+    :identifier => __FILE__
+  }
+
+  fda_account = Account.first(:code => 'FDA', :name => 'FDA')
+  raise "FDA account not present, please add it." unless fda_account
+  p.account = fda_account
+
+  k = AuthenticationKey.new
+  k.attributes = { :auth_key => Digest::SHA1.hexdigest(__FILE__) }
+  p.authentication_key = k
+
+  p.save!
+end
+
+create_op_agent unless OperationsAgent.first(:identifier => __FILE__)
 
 enqueued_and_authorized = Request.all(:is_authorized => true, :status => :enqueued, :order => [ :timestamp.asc ])
 
@@ -21,5 +45,7 @@ enqueued_and_authorized.each do |request|
     request.status = :released_to_workspace
     request.save!
   end
+
+  PackageTracker.insert_op_event __FILE__, request.ieid, "Request Released To Workspace", "request_id: #{request.id}"
 end
 
